50a8b895d29b59f2b207fe6ea78b6895e105ba95
fonzipper <fonzipper@gmail.com>
2016-03-12 19:57:19 +0300

    ns

--- grails-app/controllers/hackathonhotelbooking/RestClientController.groovy
+++ grails-app/controllers/hackathonhotelbooking/RestClientController.groovy
@@ -1,14 +1,38 @@
-
-
 package hackathonhotelbooking
+
+import groovy.json.JsonSlurper
 
 class RestClientController {
 
     def index() {}
 
-    def sendHotelsLook(BookingLookUp blu){
+    def sendHotelsLook(String params){
+        System.out.println(params);
         String req;
 
-        String response = HotelBedsHttpClient.sendRequest(req);
+        def jsn = new JsonSlurper().parseText(params);
+        BookingLookUp blu = new BookingLookUp();
+        blu.xLocation = jsn.lattitude.toDouble();
+        blu.yLocation = jsn.longitude?.toDouble();
+        blu.fulltimeCheckIn = Date.parse('mm-dd-yyyy', jsn.checkin);
+        blu.fulltimeCheckOut = Date.parse('mm-dd-yyyy', jsn.checkout);
+        blu.radius = jsn.radius?.toDouble();
+
+        blu.groupSettings = new ArrayList<>(5);
+
+
+        jsn.groups?.'*'?.each{group ->
+            BookingGroupSettings bgs = new BookingGroupSettings();
+            bgs.groupSize = group.size;
+            bgs.accommodationType = group.type;
+            bgs.accommodationSize = group.accSize;
+            bgs.stars = group.stars?.text();
+
+            blu.groupSettings.add(bgs);
+        }
+
+
+//        String response = HotelBedsHttpClient.sendRequest(req);
+        render view: '/bookingLookUp/index'
     }
 }
