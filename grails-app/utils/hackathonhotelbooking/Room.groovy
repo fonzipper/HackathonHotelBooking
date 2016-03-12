b081b207ea4da532092f5b289cb77d4ef515632d
fonzipper <fonzipper@gmail.com>
2016-03-13 01:09:05 +0300

    20:20

--- grails-app/utils/hackathonhotelbooking/Room.groovy
+++ grails-app/utils/hackathonhotelbooking/Room.groovy
@@ -1,54 +1,65 @@
 package hackathonhotelbooking
 public class Room {
-    //private int id;
-    private def roomCode;
+    private String roomCode;
     private int roomPrice;
     private Hotel roomHotelCode;
-    private def boardCode;
+    private String boardCode;
     private Integer adults;
     private Integer children;
     private int [] occupied;
-    Room [] rooms;
-    public void addRooms(/*string roomCode_I, int roomPrice_I, string boardCode_I, Hotel roomHotelCode_I, int allotment*/ RoomEntry [] roomEntries){
-        for (int i = 0; i < roomEntries.length; i++){
-            for (int j = 0; j < roomEntries[i].allotment; j++){
-                rooms.add(new Room(roomCode = roomEntries[i].roomCode_I, roomPrice = roomEntries[i].roomPrice_I, roomHotelCode = roomEntries[i].roomHotelCode_I, boardCode = roomEntries[i].boardCode_I, occupied = []))
-            }
-        }
-    }
-/*int getId(){
-return id
-}
-void setId(int id){this.id=id
-}*/
+
     public def getRoomCode(){
-return roomCode
-}
-void setRoomCode(def roomCode){this.roomCode=roomCode
-}
+        return roomCode
+    }
+    void setRoomCode(def roomCode){this.roomCode=roomCode
+    }
     public int getRoomPrice(){
-return roomPrice
-}
-void setRoomPrice(int roomPrice){this.roomPrice=roomPrice
-}
+        return roomPrice
+    }
+    void setRoomPrice(int roomPrice){this.roomPrice=roomPrice
+    }
     public Hotel getRoomHotelCode(){
-return roomHotelCode
-}
-void setRoomHotelCode(Hotel roomHotelCode){this.roomHotelCode=roomHotelCode
-}
+        return roomHotelCode
+    }
+    void setRoomHotelCode(Hotel roomHotelCode){this.roomHotelCode=roomHotelCode
+    }
     public def getBoardCode(){
-return boardCode
-}
-void setBoardCode(def boardCode){this.boardCode=boardCode
-}
+        return boardCode
+    }
+    void setBoardCode(def boardCode){this.boardCode=boardCode
+    }
     public int[] getOccupied(){
-return occupied
-}
-void setOccupied(int[] occupied){this.occupied=occupied
-}
+        return occupied
+    }
+    void setOccupied(int[] occupied){this.occupied=occupied
+    }
     public Room[] getRooms(){
-return rooms
-}
-void setRooms(Room[] rooms){this.rooms=rooms
-}
+        return rooms
+    }
+    void setRooms(Room[] rooms){this.rooms=rooms
+    }
+
+    void setBoardCode(String boardCode) {
+        this.boardCode = boardCode
+    }
+
+    void setRoomCode(String roomCode) {
+        this.roomCode = roomCode
+    }
+
+    Integer getChildren() {
+        return children
+    }
+
+    void setChildren(Integer children) {
+        this.children = children
+    }
+
+    Integer getAdults() {
+        return adults
+    }
+
+    void setAdults(Integer adults) {
+        this.adults = adults
+    }
 }
